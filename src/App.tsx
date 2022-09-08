68408be7902f44498acc54e8df25716ecb4ba9c7
Ahmet Faruk Uzunkaya <sosyal.ahmet.fuzunkaya@gmail.com>
2022-09-08 23:49:49 +0300

    eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,10 @@ import './App.css';
 import CoreTable from './components/CoreTable';
 
 export default class App extends React.Component {
-  constructor(props: any) {
-    super(props);
-  }
-
   render() {
     return (<>
       <div className="App">
-        <CoreTable docId='user3'/>
+        <CoreTable />
       </div>
     </>);
   }
--- src/components/CoreTable.tsx
+++ src/components/CoreTable.tsx
@@ -1,105 +1,186 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { TimePicker } from "antd";
-
-import { db } from '../config/firebase';
-
-import dummyData from "../data/dummy.json";
 import { doc, getDoc } from 'firebase/firestore';
-import moment from 'moment';
+import { db } from '../config/firebase';
+import { Button, TimePicker } from "antd";
+import { Table } from "antd";
+import { ColumnsType } from 'antd/lib/table';
 
-function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
-    date: number
-) {
-    return { name, calories, fat, carbs, protein, date };
-}
-
-const data = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0, new Date().getDate()),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3, new Date().getDate()),
-    createData('Eclair', 262, 16.0, 24, 6.0, new Date().getDate()),
-    createData('Cupcake', 305, 3.7, 67, 4.3, new Date().getDate()),
-    createData('Gingerbread', 356, 16.0, 49, 3.9, new Date().getDate()),
-];
-
-interface CoreTableProps {
-    docId: string;
-}
+interface CoreTableProps { }
 
 interface CoreTableState {
-    nameTitle?: string;
-    load: boolean;
+    columns: any;
+    data: any;
+
+    dateMonday: string;
+    mondayHours: any;
+
+    eployeeRow: EmployeeRow[];
+}
+
+interface EmployeeRow {
+    key: string
+    personName: string;
+    day: string;
+    dayDate: string;
+    workHours: {
+        start: string,
+        end: string,
+        totalHour: number
+    }
+}
+
+interface DataType {
+    key: string
+    day: string;
+    dayDate: string;
+    workHours: {
+        start: string,
+        end: string,
+        totalHour: number
+    }
 }
 
 export default class CoreTable extends React.Component<CoreTableProps, CoreTableState> {
     constructor(props: any) {
         super(props);
+        this.state = {
+            columns: undefined,
+            data: undefined,
+            dateMonday: "",
+            mondayHours: [],
+            eployeeRow: []
+        };
+        this.createDataGrid = this.createDataGrid.bind(this);
     }
 
-    componentDidMount = () => {
-        console.log("Tamamen Render..");
-        const userDoc = doc(db, 'users', this.props.docId);
+    componentDidMount() {
+        this.createDataGrid();
+        const userDoc = doc(db, 'schedule', "week1");
         getDoc(userDoc).then((user: any) => {
-            // user.data().name;
-            // user.data().passwd;
-            console.log(user.data().name, " - ", user.data().passwd);
+            console.log(user.data());//user.data().name, " - ", user.data().passwd);
         });
     }
 
-    render(): React.ReactNode {
+    createDataGrid() {
+        let columns: ColumnsType<DataType> = [
+            {
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
+            },
+            {
+                title: 'Monday',
+                dataIndex: 'Monday',
+                key: 'Monday',
+                render: () => (
+                    <>
+                        <TimePicker.RangePicker
+                            showSecond={false}
+                            style={{ width: "auto" }}
+                            placeholder={["S", "E"]}
+                            minuteStep={15}
+                            suffixIcon={false}
+                            separator={'-'}
+                            onChange={this.handleTimePicker}
+                        />
+                    </>
+                )
+            },
+            {
+                title: 'WEEKLY TOTAL',
+                dataIndex: 'WEEKLY TOTAL',
+                key: 'WEEKLY TOTAL',
+            },
+            {
+                title: 'Action',
+                key: 'action',
+                render: () => (
+                    <>
+                        <Button
+                            onClick={() => {
+                                console.log(this.state);
+                            }}
+                        >Save</Button>
+                    </>
+                )
+            },
+        ];
+
+        const data: DataType[] = [
+            {
+                "key": "monday",
+                "day": "monday",
+                "dayDate": "10.10.1000",
+                "workHours": {
+                    "start": "00.00",
+                    "end": "02.00",
+                    "totalHour": 8
+                }
+            }, {
+                "key": "monday",
+                "day": "monday",
+                "dayDate": "10.10.1000",
+                "workHours": {
+                    "start": "00.00",
+                    "end": "02.00",
+                    "totalHour": 8
+                }
+            }, {
+                "key": "monday",
+                "day": "monday",
+                "dayDate": "10.10.1000",
+                "workHours": {
+                    "start": "00.00",
+                    "end": "02.00",
+                    "totalHour": 8
+                }
+            }, {
+                "key": "monday",
+                "day": "monday",
+                "dayDate": "10.10.1000",
+                "workHours": {
+                    "start": "00.00",
+                    "end": "02.00",
+                    "totalHour": 8
+                }
+            }, {
+                "key": "monday",
+                "day": "monday",
+                "dayDate": "10.10.1000",
+                "workHours": {
+                    "start": "00.00",
+                    "end": "02.00",
+                    "totalHour": 8
+                }
+            }
+        ];
+
+
+        this.setState({
+            columns: columns,
+            data: data
+        })
+    }
+
+
+    handleTimePicker = (values: any) => {
+        console.log("E Budur: ", values);
+        this.setState({
+            mondayHours: values
+        });
+    }
+
+
+    render() {
         return (
             <>
-                <div style={{}}>
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                                <TableRow style={{ backgroundColor: "gray" }}>
-                                    <TableCell align="center"> KISI</TableCell>
-                                    <TableCell align="center"> GUN</TableCell>
-                                    <TableCell align="center"> GUN</TableCell>
-                                    <TableCell align="center"> GUN</TableCell>
-                                    <TableCell align="center"> GUN</TableCell>
-                                    <TableCell align="center"> GUN</TableCell>
-                                    <TableCell align="center"> GUN</TableCell>
-                                    <TableCell align="center"> GUN</TableCell>
-                                    <TableCell align="center"> TOTAL</TableCell>
-                                    <TableCell align="center"> EDIT</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {dummyData.hafta1.employees.map((data) => (
-                                    <TableRow key={data.kullaniciAd} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                        <TableCell component="th" scope="row">{data.kullaniciAd}</TableCell>
-                                        {data.workDays.forEach((val: any) => {
-                                            val.map((obj: any) => {
-                                                <TableCell align="center">
-                                                <TimePicker.RangePicker
-                                                    value={[moment(obj.monday.workHours.start, "HH.mm"), moment(data.workDays[0].monday?.workHours.end, "HH:mm")]}
-                                                    size='small'
-                                                    showSecond={false}
-                                                    minuteStep={15}
-                                                />
-                                            </TableCell>
-                                            })
-                                        })}
-                                        <TableCell align="right">{data.weekTotal}</TableCell>
-                                        <TableCell align="right">ACTION</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+                <div style={{ padding: "50px" }}>
+                    <Table
+                        columns={this.state.columns}
+                        dataSource={this.state.data}
+                        bordered={true}
+                        pagination={false}
+                    />
                 </div>
             </>
         );
