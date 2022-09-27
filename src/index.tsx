2a52d4563407991c6e1b6af2929be29c4a1f6d1b
Ahmet Faruk Uzunkaya <sosyal.ahmet.fuzunkaya@gmail.com>
2022-09-27 21:23:19 +0300

    editing

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './styles/index.css';
-import App from './components/core/App';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<App />, document.getElementById("root"));
